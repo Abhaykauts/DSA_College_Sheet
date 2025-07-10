451897d3e3eeeead00216afb3625348fd6a97a90
Abhay kauts <abhay.2226cse1152@kiet.edu>
2025-07-10 14:19:36 +0530

    Day 3rd July Done

--- Two Pointer+Sorting/15.java
+++ Two Pointer+Sorting/15.java
@@ -1,5 +1,41 @@
-package DSA_College_Sheet.Two Pointer+Sorting;
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
 
-public class 15 {
-    
+        for (int i = 0; i < n-2; i++) {
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum > 0) {
+                    k--;
+                }
+
+                else if (sum < 0) {
+                    j++;
+                }
+
+                else {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    result.add(temp);
+                    j++;
+                    k--;
+
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(k>j && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+
+        return result;
+    }
 }
--- Two Pointer+Sorting/3.java
+++
@@ -1,5 +0,0 @@
-package DSA_College_Sheet.Two Pointer+Sorting;
-
-public class 3 {
-    
-}
